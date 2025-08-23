625b41274e39d30188ea6db8d80ac55824c41683
songyouyong <fjsongyy@163.com>
2025-08-23 20:30:52 +0800

    页面调整

--- SoulPets/Data/Services/UserPreferencesService.swift
+++ SoulPets/Data/Services/UserPreferencesService.swift
@@ -32,6 +32,6 @@ class UserPreferencesService {
     
     /// 每只宠物最大照片总数
     var maxPhotosPerPet: Int {
-        return isProMember ? Int.max : 5    
+        return isProMember ? Int.max : 50
     }
 }
